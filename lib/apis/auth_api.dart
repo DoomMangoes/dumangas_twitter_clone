b6a26ccc44628efda98a9821d624f60a1ba27220
MARC DIONIS DUMANGAS <marcdionis.dumangas.20@usjr.edu.ph>
2023-07-02 21:08:17 +0800

    started auth sign up feature

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,50 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:dumangas_twitter_clone/core/core.dart';
+import 'package:fpdart/fpdart.dart';
+
+
+//Appwrite account -> handles services / retrieves data
+//Models user -> accessed data
+
+abstract class IAuthAPI{
+
+  FutureEither<model.User> signUp(
+    {
+      required String email,
+      required String password,
+    }
+  );
+
+}
+
+class AuthAPI implements IAuthAPI{
+  final Account _account;
+
+  //Named constructor cannot have this
+  AuthAPI({required Account account}): _account = account;
+
+
+  @override
+  FutureEither<model.User> signUp({
+    required String email, 
+    required String password
+    }) async {
+      try{
+        final account = await _account.create(
+          userId: ID.unique(), 
+          email: email, 
+          password: password);
+
+          return right(account);
+
+      } on AppwriteException catch (e, stackTrace){
+        return left(Failure(e.message??'Some unexpected error occured', stackTrace));
+      }
+      catch (e, stackTrace) {
+
+          return left(Failure(e.toString(), stackTrace));
+      }
+  }
+}
+
